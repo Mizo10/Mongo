190096e7a9c477ce57dc111ff0e4fdfeed91cc5c
Mizo10 <moutazabdelhamid@gmail.com>
2020-02-01 11:01:32 -0500

    views

---
+++ models/note.js
@@ -0,0 +1,20 @@
+// Require mongoose
+var mongoose = require("mongoose");
+
+// Create a schema class
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema({
+
+  // Title is of type String
+  title: String,
+
+  // Body is of type String
+  body: String
+});
+
+// Create the Note model with the NoteSchema
+var Note = mongoose.model("note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
